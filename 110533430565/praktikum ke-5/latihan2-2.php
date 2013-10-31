e9826ecf2561bb38449229b807032624637c4334
Umi Kulsum <umikulsum2408@yahoo.com>
2013-10-31 09:11:31 +0700

    Laporan 5

---
+++ 110533430565/praktikum ke-5/latihan2-2.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+        <title>Membuat Database Latihan 2</title>
+</head>
+<body>
+<?php 
+require_once("koneksi.php");
+
+$db_name="myweb";
+$res=mysql_query("
+        create table mahasiswa
+        (nim varchar(12) not null,
+        nama varchar(40) not null,
+        alamat varchar(100),
+        primary key(nim)
+                )ENGINE=MyISAM
+        ");
+if ($res) {
+        echo "Table  Created";
+        mysql_close();
+}
+else
+{
+        echo mysql_error();
+}
+ ?>
+
+ 
+</body>
+</html>
---
+++ 110533430565/praktikum ke-5/latihan2.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+        <title>Membuat Database Latihan 2</title>
+</head>
+<body>
+<?php 
+require_once("koneksi.php");
+
+$db_name="myweb";
+$res=mysql_query("CREATE database ". $db_name);
+if ($res) {
+        echo "Database ". $db_name. " Created";
+}
+else
+{
+        echo mysql_error();
+}
+ ?>
+
+ 
+</body>
+</html>
